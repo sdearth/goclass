4a221fc016f4ceeabc8c7425184c57673315af73
Steve Dearth <stevedearth@us.ibm.com>
2021-02-12 09:20:35 -0500

    initial commit

---
+++ helloworld/main.go
@@ -0,0 +1,9 @@
+//must have a package name of main in order to build an executable
+package main
+
+//can get doc from https://golang.org/pkg
+import "fmt"
+
+func main() {
+	fmt.Println("hi there")
+}
